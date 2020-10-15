6f4cef72a969cd4c0be0effe6bc2ad0829953566
mriang <72933422+mriang@users.noreply.github.com>
2020-10-16 00:59:47 +0800

    Add files via upload

---
+++ SeleniumJavaFramework/src/test/java/AmazonWebsiteTest1.java
@@ -0,0 +1,76 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class AmazonWebsiteTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testAmazonWebsite() throws Exception {
+    driver.get("https://www.amazon.com.au/");
+    driver.findElement(By.id("twotabsearchtextbox")).click();
+    driver.findElement(By.id("twotabsearchtextbox")).clear();
+    driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Disney: The Lion King Classic Collection");
+    driver.findElement(By.name("site-search")).submit();
+    driver.findElement(By.xpath("//img[@alt='Disney: The Lion King Classic Collection']")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
